15f96a2f8b3ad3592ce6cf6c11a2644c155f7af4
Chris Zappia <christopher.zappia@gmail.com>
2021-09-02 23:13:37 +1000

    added generator for the cards for each card type

---
+++ poker-app/poker-app/src/gameutil/CardGenerator.ts
@@ -0,0 +1,36 @@
+import { Card } from "semantic-ui-react";
+import { CardType } from "../data";
+
+
+export interface DisplayCard {
+    displayValue: string,
+    dbValue: string,
+}
+
+const MODIFIED_FIBONACCI_CARDS: DisplayCard[] = [
+    "0", "0.5", "1", "2", "3", "5", "8", "13", "20","40","100","?"].map(toDisplayCard);
+
+const FIBONACCI_CARDS: DisplayCard[] = [
+    "0", "0.5", "1", "2", "3", "5", "8", "13", "21", "34", "55", "89", "?"].map(toDisplayCard);
+
+const T_SHIT_SIZE_CARDS: DisplayCard[] = [
+    "XS", "S", "M", "L", "XL", "?"].map(toDisplayCard);
+
+
+export function getCardsForType(cardType: CardType): DisplayCard[] {
+    switch(cardType) {
+        case(CardType.FIBONACCI): {
+            return FIBONACCI_CARDS
+        }
+        case(CardType.FIBONACCI_MODIFIED): { 
+            return MODIFIED_FIBONACCI_CARDS;
+        }
+        case(CardType.T_SHIRT_SIZES): {
+            return T_SHIT_SIZE_CARDS;
+        }
+    }
+}
+
+function toDisplayCard(cardValue: string): DisplayCard {
+    return {dbValue: cardValue, displayValue: cardValue} as DisplayCard;
+}
