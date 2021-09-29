0fdc24416223863f529c6d8d430b156f4a6b182e
Chris Zappia <christopher.zappia@gmail.com>
2021-09-29 23:26:54 +1000

    silence the last warning (revisit later)

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -2,5 +2,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
+// TODO figure out why this is cooked
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
