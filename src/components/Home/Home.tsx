c7864e5f2659a9a7e923433e7a60b99be1f25081
Chris Zappia <christopher.zappia@gmail.com>
2021-09-29 22:14:11 +1000

    Create the form for creating a new planning poker game

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import { useHistory } from "react-router-dom";
+import { Button, DropdownProps, Form, Select } from 'semantic-ui-react';
 import { CardType } from '../../data';
 import { createNewGame, firebaseInit } from '../../database';
 
@@ -7,17 +8,40 @@ function Home() {
 
     let history = useHistory();
 
+    const [gameName, setGameName] = useState<string>("Planning Poker Game");
+    const [cardType, setCardType] = useState<CardType>(CardType.FIBONACCI_MODIFIED);
+
+    const cardTypes = [
+        {key: CardType.FIBONACCI, value: CardType.FIBONACCI, text: "Fibonacci"},
+        {key: CardType.FIBONACCI_MODIFIED, value: CardType.FIBONACCI_MODIFIED, text: "Modified Fibonacci"},
+        {key: CardType.T_SHIRT_SIZES, value: CardType.T_SHIRT_SIZES, text: "T-Shirt Sizes"},
+    ];
+
     function initGame(): void {
         firebaseInit();
-        const newGameId = createNewGame("Test Name", CardType.FIBONACCI_MODIFIED);
+        const newGameId = createNewGame(gameName, cardType);
       
         history.push("game/"+ newGameId);
     }
 
+    function dropDownHandler(event: SyntheticEvent, data: DropdownProps) {
+        setCardType(data.value as CardType);
+    }
+
     return (
         <>
-            <h1>Poker App</h1>
-            <button onClick={initGame}>New Game</button>
+            <h1>Planning Poker App</h1>
+            <Form>
+                <Form.Field>
+                    <label>Game Name</label>
+                    <input defaultValue={gameName} onChange={e => setGameName(e.target.value)} />
+                </Form.Field>
+                <Form.Field>
+                    <label>Card Type</label>
+                    <Select options={cardTypes} defaultValue={cardType} onChange={dropDownHandler} />
+                </Form.Field>
+                <Button onClick={initGame} type='submit'>New Game</Button>
+            </Form>
         </>
     );
 
